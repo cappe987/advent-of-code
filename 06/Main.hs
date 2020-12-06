30a0366414917f6d20170009ec50f34fa75cebc2
Casper Andersson <andersson.casper99@gmail.com>
2020-12-06 10:47:47 +0100

    Added one-liner for day 6

--- 06/Main.hs
+++ 06/Main.hs
@@ -12,8 +12,16 @@ solve_1 = foldl (\acc x -> acc + length (nub x)) 0
 solve_2 :: [[String]] -> Int
 solve_2 = foldl (\acc x -> acc + length (foldl1 intersect x)) 0
 
+
+solve_1_1 = print . foldl (\acc x -> acc + length (nub x)) 0 . map concat . filter (/= [""]) . groupBy (\a b -> a /= "" && b /= "") . lines =<< readFile "input.txt"
+
+solve_1_2 = print . foldl (\acc x -> acc + length (foldl1 intersect x)) 0 . filter (/= [""]) . groupBy (\a b -> a /= "" && b /= "") . lines =<< readFile "input.txt"
+
 main :: IO ()
 main = do 
   input <- readFile "input.txt"
   print $ solve_1 $ map concat $ parse input
   print $ solve_2 $ parse input
+
+  solve_1_1 
+  solve_1_2
