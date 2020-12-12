e90a4e732cdaef5c5a99401960d67ce3d8c076a6
Casper Andersson <andersson.casper99@gmail.com>
2020-12-12 23:43:13 +0100

    Refactored day 12 part 1

--- 12/Main.hs
+++ 12/Main.hs
@@ -1,50 +1,30 @@
 module Main where
 
+type Boat = ((Int, Int), (Int, Int))
 
-data Instruction = F Int | D Dir Int | L Int | R Int deriving Show
+------------- Part 1 ----------------
+moveForward :: Boat -> Int -> Boat
+moveForward (p, (dx,dy)) n = (foldl (\(x, y) _ -> (x+dx, y+dy)) p [1..n], (dx,dy))
 
-data Dir = East | West | South | North deriving Show
+turnLeft :: Boat -> Int -> Boat
+turnLeft  (p, (dx,dy)) n = if n == 0 then (p, (dx,dy)) else turnLeft  (p, (dy, -dx)) (n-1)
 
-data Boat = Boat Dir (Int, Int) deriving Show
+turnRight :: Boat -> Int -> Boat
+turnRight (p, (dx,dy)) n = if n == 0 then (p, (dx,dy)) else turnRight (p, (-dy, dx)) (n-1)
 
-turnRight boat degrees = foldl turn boat [1..degrees `div` 90]
-  where turn (Boat East  p) _ = Boat South p
-        turn (Boat South p) _ = Boat West p
-        turn (Boat West  p) _ = Boat North p
-        turn (Boat North p) _ = Boat East p
+moveDir :: (Int, Int) -> Boat -> Int -> Boat
+moveDir (dx,dy) ((x,y), d) n = 
+  if n == 0 then ((x,y), d) else moveDir (dx, dy) ((x+dx, y+dy), d) (n-1)
 
-turnLeft boat degrees = foldl turn boat [1..degrees `div` 90]
-  where turn (Boat East  p) _ = Boat North p
-        turn (Boat South p) _ = Boat East p
-        turn (Boat West  p) _ = Boat South p
-        turn (Boat North p) _ = Boat West p
-
-moveForward (Boat East  (x,y)) n = Boat East  (x+n, y)
-moveForward (Boat South (x,y)) n = Boat South (x, y+n)
-moveForward (Boat West  (x,y)) n = Boat West  (x-n, y)
-moveForward (Boat North (x,y)) n = Boat North (x, y-n)
-
-moveDir (Boat d (x,y)) East  n = Boat d (x+n,y)
-moveDir (Boat d (x,y)) South n = Boat d (x,y+n)
-moveDir (Boat d (x,y)) West  n = Boat d (x-n,y)
-moveDir (Boat d (x,y)) North n = Boat d (x,y-n)
-
-parse :: [String] -> [Instruction]
-parse = 
-  fmap parseLine 
-  where parseLine ('F':xs) = F (read xs)
-        parseLine ('N':xs) = D North (read xs)
-        parseLine ('S':xs) = D South (read xs)
-        parseLine ('W':xs) = D West  (read xs)
-        parseLine ('E':xs) = D East  (read xs)
-        parseLine ('L':xs) = L (read xs)
-        parseLine ('R':xs) = R (read xs)
-
-solve_1 [] (Boat _ (x,y)) = abs x + abs y
-solve_1 (F n:xs) boat = solve_1 xs (moveForward boat n)
-solve_1 (L n:xs) boat = solve_1 xs (turnLeft  boat n)
-solve_1 (R n:xs) boat = solve_1 xs (turnRight boat n)
-solve_1 (D d n:xs) boat = solve_1 xs (moveDir boat d n)
+solve_1 :: [String] -> Boat -> Int
+solve_1 [] ((x,y), _) = abs x + abs y
+solve_1 (('F':n):xs) boat = solve_1 xs (moveForward boat (read n))
+solve_1 (('L':n):xs) boat = solve_1 xs (turnLeft  boat (read n `div` 90))
+solve_1 (('R':n):xs) boat = solve_1 xs (turnRight boat (read n `div` 90))
+solve_1 (('N':n):xs) boat = solve_1 xs (moveDir ( 0,-1) boat (read n))
+solve_1 (('S':n):xs) boat = solve_1 xs (moveDir ( 0, 1) boat (read n))
+solve_1 (('E':n):xs) boat = solve_1 xs (moveDir ( 1, 0) boat (read n))
+solve_1 (('W':n):xs) boat = solve_1 xs (moveDir (-1, 0) boat (read n))
 
 
 ------------- Part 2 ----------------
@@ -67,11 +47,10 @@ solve_2 (('S':n):xs) boat (px, py) = solve_2 xs boat (px, py + read n)
 solve_2 (('W':n):xs) boat (px, py) = solve_2 xs boat (px - read n, py)
 solve_2 (('E':n):xs) boat (px, py) = solve_2 xs boat (px + read n, py)
 
+------------- Main ----------------
 main :: IO ()
 main = do 
   input <- lines <$> readFile "input.txt"
   
-  -- print input
-  print $ solve_1 (parse input) (Boat East (0,0))
+  print $ solve_1 input ((0,0), (1,0))
   print $ solve_2 input (0,0) (10, -1)
-  putStrLn ""
