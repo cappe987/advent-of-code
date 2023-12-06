93788e72898668dcb1f97218573465461be413e2
Casper Andersson <casper.casan@gmail.com>
2023-12-06 18:57:48 +0100

    Optimized day 03 again
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -15,7 +15,7 @@ def get_neighbours(i, j, grid):
 def to_number(num, grid):
     return int("".join(map(lambda ij: grid[ij[0]][ij[1]], num)))
 
-def part_1(grid):
+def part_1(grid, numbers):
     tot = 0
     for n in numbers:
         for ij in n:
@@ -24,60 +24,36 @@ def part_1(grid):
                 break
     return tot
 
-def get_num_at(grid, i, j):
-    coords = []
-    if in_range(i, j-1, grid) and grid[i][j-1].isdigit():
-        if in_range(i,j-2, grid) and grid[i][j-2].isdigit():
-            coords.append((i,j-2))
-        coords.append((i,j-1))
-    coords.append((i,j))
-    if in_range(i, j+1, grid) and grid[i][j+1].isdigit():
-        coords.append((i,j+1))
-        if in_range(i, j+2, grid) and grid[i][j+2].isdigit():
-            coords.append((i,j+2))
-    return to_number(coords, grid)
-
-def neigh_digit(grid, i,j):
-    digits = []
-    if grid[i-1][j-1].isdigit():
-        digits.append((i-1,j-1))
-        if not grid[i-1][j].isdigit() and grid[i-1][j+1].isdigit():
-            digits.append((i-1,j+1))
-    elif grid[i-1][j].isdigit():
-        digits.append((i-1,j))
-    elif grid[i-1][j+1].isdigit():
-        digits.append((i-1,j+1))
-
-    if grid[i+1][j-1].isdigit():
-        digits.append((i+1,j-1))
-        if not grid[i+1][j].isdigit() and grid[i+1][j+1].isdigit():
-            digits.append((i+1,j+1))
-    elif grid[i+1][j].isdigit():
-        digits.append((i+1,j))
-    elif grid[i+1][j+1].isdigit():
-        digits.append((i+1,j+1))
-
-    if grid[i][j-1].isdigit():
-        digits.append((i,j-1))
-    if grid[i][j+1].isdigit():
-        digits.append((i,j+1))
-
-    if len(digits) == 2:
-        return prod(map(lambda x: get_num_at(grid, x[0], x[1]), digits))
-
-    return 0
-
-def part_2(grid):
+def part_2(grid, numbers):
     tot = 0
     gears = [(i,j) for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == '*']
     for (i,j) in gears:
-            # Optimized solution
-            tot += neigh_digit(grid, i, j)
+            neigh_idx = list(filter(lambda x: grid[x[0]][x[1]].isdigit(), get_neighbours(i, j, grid)))
+            neighs = [n for n in numbers if any(map(lambda x: (x[0],x[1]) in neigh_idx, n))]
+            if len(neighs) == 2:
+                tot += to_number(neighs[0], grid) * to_number(neighs[1], grid)
+    return tot
 
-            # neigh_idx = list(filter(lambda x: grid[x[0]][x[1]].isdigit(), get_neighbours(i, j, grid)))
-            # neighs = [n for n in numbers if any(map(lambda x: (x[0],x[1]) in neigh_idx, n))]
-            # if len(neighs) == 2:
-                # tot += to_number(neighs[0], grid) * to_number(neighs[1], grid)
+def part_2_opt(grid, numbers):
+    gears = {}
+    tot = 0
+    for num in numbers:
+        found = False
+        for digit in num:
+            for neigh in get_neighbours(digit[0],digit[1], grid):
+                if grid[neigh[0]][neigh[1]] == '*':
+                    found = True
+                    if neigh in gears:
+                        gears[neigh].append(num)
+                    else:
+                        gears[neigh] = [num]
+                    break
+            if found:
+                break
+
+    for gear in gears:
+        if len(gears[gear]) == 2:
+            tot += prod(map(lambda x: to_number(x, grid), gears[gear]))
     return tot
 
 grid = list(map(lambda x: x.strip(), data))
@@ -93,6 +69,6 @@ for i in range(len(grid)):
     if len(num) > 0:
         numbers.append(num)
 
-print(f"Part 1: {part_1(grid)}")
-print(f"Part 2: {part_2(grid)}")
+print(f"Part 1: {part_1(grid, numbers)}")
+print(f"Part 2: {part_2_opt(grid, numbers)}")
 
